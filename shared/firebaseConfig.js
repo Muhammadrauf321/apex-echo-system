65b2772b1e1657fdc70289ab75d7826d27f44ea5
Muhammad Rauf <muhammadraufbaloch6@gmail.com>
2026-09-06 13:22:52 -0700

    Add shared Firebase SDK config for apex-echo-system

---
+++ shared/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Firebase configuration for Apex Echo System
+export const firebaseConfig = {
+  apiKey: "AIzaSyA6zw52EVPzuS4TbsqmYfhvEv3LI-S6pJM",
+  authDomain: "apex-echo-system.firebaseapp.com",
+  projectId: "apex-echo-system",
+  storageBucket: "apex-echo-system.firebasestorage.app",
+  messagingSenderId: "435515195879",
+  appId: "1:435515195879:web:ca2a63f06ebb82da90bd42"
+};
+
+export default firebaseConfig;
